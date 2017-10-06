87abb82768571ee169c12f3fb1c3a6171eeca6ea
bhudnell1240 <bthudnell@gmail.com>
2017-10-06 16:35:37 -0700

    added some database methods

--- DataBaseAdaptor.php
+++ DataBaseAdaptor.php
@@ -1,46 +1,68 @@
-<?php
-class DatabaseAdaptor {
-	private $DB;
-
-	public function __construct() {
-		$db = 'mysql:dbname=quotes;host=127.0.0.1';
-		$user = 'root';
-		$password = '';
+ <?php
+	
+	// to use methods, call: require_once './DataBaseAdaptor.php';
+	class DatabaseAdaptor {
+		private $DB;
+		public function __construct() {
+			$db = 'mysql:dbname=myDB;host=ptpa.c2ihxd5ursch.us-west-1.rds.amazonaws.com';
+			$user = 'root2';
+			$password = '12345678';
 			
-		try {
-			$this->DB = new PDO($db, $user, $password);
-			$this->DB->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		} catch (PDOException $e) {
-			echo ('Error establishing connection');
-			exit();
+			try {
+				$this->DB = new PDO ( $db, $user, $password );
+				$this->DB->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+			} catch ( PDOException $e ) {
+				echo ('Error establishing Connection');
+				exit ();
+			}
+		}
+		// $trainer is the variable used in the belongs_to field for the client
+		public function getClientsAsArray($trainer) {
+			$stmt = $this->DB->prepare ( "SELECT * FROM clients WHERE belongs_to=:trainer" );
+			$stmt->bindParam ( 'trainer', $trainer );
+			$stmt->execute ();
+			return $stmt->fetchAll ( PDO::FETCH_ASSOC );
+		}
+		// for login page
+		// requires using session_start();
+		public function login() {
+			$_SESSION ['user'] = $user;
+			$_SESSION ['login'] = true;
+		}
+		// for login page
+		public function logout() {
+			$_SESSION ['login'] = false;
+		}
+		// used to see if trainer username already exists.
+		// If the trainer username already exists return false
+		public function canRegisterTrainer($user) {
+			$stmt = $this->DB->prepare ( "SELECT user_name FROM trainers" );
+			$stmt->execute ();
+			$userArray = $stmt->fetchAll ( PDO::FETCH_COLUMN );
+			foreach ( $userArray as $name ) {
+				if (in_array ( $user, $userArray ))
+					return false;
+			}
+			return true;
+		}
+		// used to register a new trainer
+		public function registerTrainer($user, $pwd, $email) {
+			$hashed_pwd = password_hash ( $pwd, PASSWORD_DEFAULT );
+			$stmt = $this->DB->prepare ( "INSERT INTO trainers (user_name, password, email) values(:user, :password, :email)" );
+			$stmt->bindParam ( 'user', $user );
+			$stmt->bindParam ( 'password', $hashed_pwd );
+			$stmt->bindParam ( 'email', $email );
+			$stmt->execute ();
+		}
+		// call before logging in. Checks if the password is correct.
+		public function isPasswordVerified($user, $pwd) {
+			$stmt = $this->DB->prepare ( "SELECT password FROM trainers WHERE user_name=:user" );
+			$stmt->bindParam ( 'user', $user );
+			$stmt->execute ();
+			$hash = $stmt->fetch ();
+			return password_verify ( $pwd, $hash [0] );
 		}
 	}
-
-	public function getQuotesArray() {
-		$stmt = $this->DB->prepare("SELECT * FROM quotations ORDER BY rating DESC, added DESC");
-		$stmt->execute();
-		return  $stmt->fetchAll(PDO::FETCH_ASSOC);
-	}
 	
-	public function add($quote, $author){
-		$stmt = $this->DB->prepare("INSERT INTO quotations(id, added, quote, author, rating)
-				values(id, now(), :quote, :author, 0)");
-		$stmt->bindParam('quote', $quote);
-		$stmt->bindParam('author', $author);
-		$stmt->execute();
-	}
-		
-	public function incrementRating($id){
-		$stmt = $this->DB->prepare("UPDATE quotations SET rating=rating+1 WHERE id=:id");
-		$stmt->bindParam('id', $id);
-		$stmt->execute();
-	}
-	
-	public function decrementRating($id){
-		$stmt = $this->DB->prepare("UPDATE quotations SET rating=rating-1 WHERE id=:id");
-		$stmt->bindParam('id', $id);
-		$stmt->execute();
-	}
-}
-$myDatabaseFunctions = new DatabaseAdaptor();
-?>
+	$myDatabaseFunctions = new DatabaseAdaptor ();
+	?>
